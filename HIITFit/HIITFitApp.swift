ef6f55b5ea9a5cc50db9c842d8897102f0409d8b
Кириченко Дмитрий Павлович <Dpkirichenko@alfabank.ru>
2024-09-30 22:37:45 +0300

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Dmitry Kirichenko on 30.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
